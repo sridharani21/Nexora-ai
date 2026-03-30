9680a29a237b151c072ee07362ffa8eb16f7545a
SriDharani <sridharani916@gmail.com>
2026-03-30 21:19:07 +0530

    Fix courseRecommendActions import and aliases

---
+++ actions/courseRecommendActions.js
@@ -0,0 +1,11 @@
+// D:\nexora-ai\my-app\actions\courseRecommendActions.js
+
+export function getCourses() {
+  // TODO: Add your logic to fetch courses
+  return [];
+}
+
+export function getPastRecommendations() {
+  // TODO: Add your logic for past recommendations
+  return [];
+}
