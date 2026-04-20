8aea2e651b466aa3ba42946d82e1622c39519d83
SriDharani <sridharani916@gmail.com>
2026-04-20 16:14:58 +0530

    fix inngest endpoint final

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,18 +1,30 @@
-// app/api/inngest/route.js
+// // app/api/inngest/route.js
+
+// import { inngest } from "@/lib/inngest/client";
+// import { serve } from "inngest/next";
+
+// // Existing function
+// import { generateIndustryInsights } from "@/lib/inngest/functions";
+
+// // New course recommendation functions
+// import {  generateCourseRecommendations } from "@/lib/inngest/functions";
+
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     generateIndustryInsights,
+//     generateCourseRecommendations
+//   ],
+// });
+
+
+export const dynamic = "force-dynamic";
 
 import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
+import { inngestFunctions } from "@/lib/inngest/functions";
 
-// Existing function
-import { generateIndustryInsights } from "@/lib/inngest/functions";
-
-// New course recommendation functions
-import {  generateCourseRecommendations } from "@/lib/inngest/functions";
-
-export const { GET, POST, PUT } = serve({
+export const { GET, POST } = serve({
   client: inngest,
-  functions: [
-    generateIndustryInsights,
-    generateCourseRecommendations
-  ],
+  functions: inngestFunctions,
 });
