df53b80baa81fe7d9fce3f222fb14a12e87ff635
SriDharani <sridharani916@gmail.com>
2026-04-20 15:51:12 +0530

    fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,18 @@
-import { serve } from "inngest/next";
+// app/api/inngest/route.js
+
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights, generateCourseRecommendations } from "@/lib/inngest/functions";
+import { serve } from "inngest/next";
+
+// Existing function
+import { generateIndustryInsights } from "@/lib/inngest/functions";
+
+// New course recommendation functions
+import {  generateCourseRecommendations } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights, generateCourseRecommendations],
+  functions: [
+    generateIndustryInsights,
+    generateCourseRecommendations
+  ],
 });
